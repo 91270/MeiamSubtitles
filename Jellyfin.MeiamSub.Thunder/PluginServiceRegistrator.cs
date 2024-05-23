f82702f844a675c59cfb65b8d2d5b2cac000c413
Meiam <91270@qq.com>
2024-05-23 19:51:52 +0800

    解决 Jellyfin 新版调整依赖注入

---
+++ Jellyfin.MeiamSub.Thunder/PluginServiceRegistrator.cs
@@ -0,0 +1,20 @@
+﻿using MediaBrowser.Controller.Plugins;
+using MediaBrowser.Controller.Subtitles;
+using MediaBrowser.Controller;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Jellyfin.MeiamSub.Thunder
+{
+    public class PluginServiceRegistrator : IPluginServiceRegistrator
+    {
+        public void RegisterServices(IServiceCollection serviceCollection, IServerApplicationHost applicationHos)
+        {
+            serviceCollection.AddSingleton<ISubtitleProvider, ThunderProvider>();
+        }
+    }
+}
